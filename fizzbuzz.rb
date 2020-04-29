6e31d58f705d189eef2f58cbe1767d20f2cd0c22
Luis Reyes <luisreyesxv@yahoo.com>
2020-04-29 05:09:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(number)
+  if number.modulo(3)==0 && number.modulo(5)==00
+    "FizzBuzz"
+  elsif number.modulo(5)==0
+    "Buzz"
+  elsif number.modulo(3)==0
+    "Fizz"
+  else nil      
+  end  
 end
